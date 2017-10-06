defdca1d07387c1365776dd197ae524bf32566ae
c.c <469770982@qq.com>
2017-10-06 14:01:40 +0800

    clean

--- static/homePage/js/test.js
+++
@@ -1,9 +0,0 @@
-var array = [1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7];
-
-var newArray = Array.from(new Set(array));
-
-var dom = document.getElementById('array');
-
-console.log(dom);
-
-dom.innerHTML = newArray.join(',');
